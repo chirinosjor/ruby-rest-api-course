e56a2b6f2fca4b3887ff25718bc04e1e4959392a
Jorge Chirinos <chirinosjor@gmail.com>
2021-12-26 16:44:20 -0300

    first route added

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe '/articles routes' do
   it 'routes to articles#index' do
-    expect(get '/articles').to route_to('articles#index')  
+    expect(get '/articles').to route_to('articles#index') 
   end
 end
